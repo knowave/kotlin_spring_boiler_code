d5d8af10e04063e0bf2b0d1ade9fa7c37da45fc7
sije <taemin@sijeui-MacBookAir.local>
2025-09-19 15:03:32 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlin_spring_boiler_code/KotlinSpringBoilerCodeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_spring_boiler_code
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBoilerCodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBoilerCodeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlin_spring_boiler_code/KotlinSpringBoilerCodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_spring_boiler_code
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBoilerCodeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
